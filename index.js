54bdfac5ec053314f89b50c6645f608b95a976c2
Masayuki Komatsu <komatsu-m@gnavi.co.jp>
2017-06-01 17:35:48 +0900

    fixed search code

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 module.exports = (array, target) => {
-  var pos = Math.floor(array.length / 2);
-  for(var i=0; i < array.length; i++){
-    if (target === array[pos]){
+  let left  = 0;
+  let right = array.length;
+  
+  while(left < right){
+    mid = (left + right) / 2;
+    mid = Math.floor(mid);
+    if (target === array[mid]){
       return { 
-	index: pos, 
-	item: array[pos]
+	index: mid, 
+	item: array[mid]
       }
-    }else if(array[pos] < target){     
-      pos = Math.floor(pos * 2);
-    }else if(array[pos] > target){
-      pos = Math.floor(pos / 2);
+    }else if(array[mid] > target){     
+      right = mid;
+    }else{
+      left = mid + 1;
     }
   }
   return "not find"
--- test/binarySearch.spec.js
+++ test/binarySearch.spec.js
@@ -4,8 +4,7 @@ var assert = require('assert')
 describe('binarySearch test', () => {
   it('test index', () => {  
     const list = [1, 5, 32, 45, 51, 67, 79, 87, 99, 120]
-    const target = 51
-    console.dir(binarySearch);
+    const target = 51;
     const result = binarySearch(list, target)
     assert.equal(result.item, 51)
   })
