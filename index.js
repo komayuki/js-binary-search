dcd83678eb7c7cc61c58e964d322de950479ec07
Masayuki Komatsu <info@notsleeeping.com>
2017-06-29 13:48:17 +0900

    test

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-module.exports = (list, target) => {
+var bSearch = {}
+
+bSearch.search = function (list, target) {
   if (typeof list !== "object") {
     return "type error"
   }
 
   let left  = 0;
   let right = list.length;
-  
+
   while(left < right){
     mid = (left + right) / 2;
     mid = Math.floor(mid);
     if (target === list[mid]){
-      return { 
-	index: mid, 
+      return {
+	index: mid,
 	item: list[mid]
       }
-    }else if(list[mid] > target){     
+    }else if(list[mid] > target){
       right = mid;
     }else{
       left = mid + 1;
@@ -22,3 +24,31 @@ module.exports = (list, target) => {
   }
   return "not found"
 }
+
+bSearch.search_in_associative = function (list, key, target) {
+  if (typeof list !== "object") {
+    return "type error"
+  }
+
+  let left  = 0;
+  let right = list.length;
+
+  while(left < right){
+    mid = (left + right) / 2;
+    mid = Math.floor(mid);
+    if (target === list[mid][key]){
+      return {
+        index: mid,
+        item: list[mid][key]
+      }
+    }else if(list[mid][key] > target){
+      right = mid;
+    }else{
+      left = mid + 1;
+    }
+  }
+  return "not found"
+}
+
+
+module.exports = bSearch
--- test/binarySearch.spec.js
+++ test/binarySearch.spec.js
@@ -1,26 +1,40 @@
-const binarySearch = require('../index.js');
+const bSearch = require('../index.js');
 var assert = require('assert')
 
 describe('binarySearch test', () => {
-  it('test index', () => {  
+  it('test index', () => {
     const list = [1, 5, 32, 45, 51, 67, 79, 87, 99, 120]
     const target = 51;
-    const result = binarySearch(list, target)
+    const result = bSearch.search(list, target)
+
     assert.equal(result.item, 51)
   }),
 
   it('reverse array', () => {
     let list = [1, 5, 32, 45, 51, 67, 79, 87, 99, 120]
     list = list.reverse()
-    const target = 99;       
-    const result = binarySearch(list, target)
+    const target = 99;
+    const result = bSearch.search(list, target)
     assert.equal(result, "not found")
+  })
+
+  it ('binary search in associative_array', () => {
+    let list = [
+      { id: 1},
+      { id: 29},
+      { id: 45},
+      { id: 66},
+      { id: 77},
+      { id: 89},
+    ]
+    const result = bSearch.search_in_associative(list, 'id', 77)
+    assert.equal(result.index, 4)
   })
 
   it('check type', () => {
     const str = "str"
-    const target = 99;       
-    const result = binarySearch(str, target)
+    const target = 99;
+    const result = bSearch.search(str, target)
     assert.equal(result, "type error")
   })
 })
